3283a121d85dd9a0ebef845472699ec3628d7275
chenag <23110134@qq.com>
2016-12-14 23:22:14 +0800

    m

---
+++ src/js/n++_++n.js
@@ -0,0 +1,10 @@
+/**
+ * Created by cag on 2016/12/14.
+ */
+var m = 0;
+console.log(m++); // 先输出m的值，再加1 输出：0
+console.log('m = ' + m); // m = 1
+
+var n = 0;
+console.log(++n); // 先加1，再输出m的值 输出：1
+console.log('n = ' + n); // n = 1
